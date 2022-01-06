81229e4c9b9979d77b75837643a4ba11c1b860cf
JavaKaran <sharmakaran12092001@gmail.com>
2022-01-06 21:26:58 +0530

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="footer-content">
+                    <p>© Karan Sharma</p>
+                    <a
+                        href="https://github.com/JavaKaran/moviepedia"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Repository
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
